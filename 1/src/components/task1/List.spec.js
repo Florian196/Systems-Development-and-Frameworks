e053e9ff18631d9f746c8366e5bb3f69adb24b08
Florian <s0567802@htw-berlin.de>
2019-11-13 10:42:29 +0100

    edit describe

--- 1/src/components/task1/List.spec.js
+++ 1/src/components/task1/List.spec.js
@@ -1,9 +1,10 @@
 import { mount } from '@vue/test-utils';
 import List from './List';
 
-
+describe("List", () => {
     test('is a Vue instance', () => {
         const wrapper = mount(List)
         expect(wrapper.isVueInstance()).toBeTruthy()
-    })
+    });
+});
 
