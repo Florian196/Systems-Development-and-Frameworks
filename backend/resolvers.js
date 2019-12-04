f58540302d0f96eb5304cdf0c03a2d76d180dd9d
Florian <s0567802@htw-berlin.de>
2019-12-04 11:33:14 +0100

    first JWT changes pairprogramming with the whole team

--- backend/resolvers.js
+++ backend/resolvers.js
@@ -1,3 +1,6 @@
+const jwt = require('jsonwebtoken');
+const AuthenticationError = require('apollo-server-errors');
+const {decryptedToken} = require('./jwtDecoder');
 const todos = [
     {
       title: 'SDF - Task 3',
@@ -6,26 +9,60 @@ const todos = [
       title: 'SDF - Task 2',
     },
   ];
+  const users = [
+    {
+      username: "user1",
+      password: "12345"
+    },
+    {
+      username: "user2",
+      password: "54321"
+    }
+  ];
 
 const resolvers = {
     Query: {
-      todos: () => todos,
-    },
+      todos: (object, params) => {
+        const decrypted = decryptedToken(params.token);
+        return todos;
+    }
+  },
     Mutation: {
         addToDo: (object, input) =>{
-            todos.push({
-               title: input.title,
-            });
+          const decrypted = decryptedToken(params.token);
+          todos.push({
+              title: input.title,
+          });
         },
         deleteToDo: (object, input) =>{
-            todos.splice(input.index, 1);
+          const decrypted = decryptedToken(params.token);
+          todos.splice(input.index, 1);
         },
         updateToDo: (object, input) => {
-            todos[input.index].title = input.title;
+          const decrypted = decryptedToken(params.token);
+          todos[input.index].title = input.title;
+        },
+      loginUser: (object, params)  => {
+
+        let user  = users.find(
+          user => user.username === params.username );
+        if(user !== undefined){
+          throw new AuthenticationError(
+            "Username undefined"
+          );
         }
+
+        if(user.password !== params.password){
+          throw new AuthenticationError(
+            "Wrong password"
+          )
+        }
+        return {token : jwt.sign(user, "12345")};
+      }
     }
   };
 
 
 module.exports.resolvers = resolvers;
+
   
--- backend/typeDefs.js
+++ backend/typeDefs.js
@@ -7,12 +7,32 @@ const typeDefs = gql`
 
   type Query {
     todos: [todos]
+    users: [User]
+  }
+  type User {
+    name: String!
+    password: String!
+    id: Int
   }
   type Mutation {
       addToDo(title: String!): [todos]
       deleteToDo(index: Int!): [todos]
       updateToDo(title: String!, index: Int!): [todos]
-  }
+      signupUser(data: UserCreateInput!) : AuthPayLoad!
+      loginUser(data: UserLoginInput!): AuthPayLoad!  
+    }
+      
+  input UserCreateInput {
+      username: String!
+      password: String!
+    }
+    input UserLoginInput {
+      username: String!
+      password: String!
+    }
+    type AuthPayLoad {
+      token: String!
+    }
 `;
 
 module.exports.typeDefs = typeDefs;
